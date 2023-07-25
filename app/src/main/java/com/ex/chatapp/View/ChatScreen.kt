62257e7d0d1dd2607c0e17bcdb4f6397c96fd2a3
hakanAkduman <hakanakduman2002@gmail.com>
2023-07-25 22:54:39 +0300

    chat screen design has been changed

--- app/src/main/java/com/ex/chatapp/View/ChatScreen.kt
+++ app/src/main/java/com/ex/chatapp/View/ChatScreen.kt
@@ -3,7 +3,9 @@ package com.ex.chatapp.View
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,12 +20,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
@@ -35,6 +35,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
@@ -47,7 +48,6 @@ import coil.compose.rememberAsyncImagePainter
 import com.ex.chatapp.Model.Message
 import com.ex.chatapp.R
 import com.ex.chatapp.ViewModel.ChatScreenViewModel
-import com.ex.chatapp.ViewModel.MainScreenViewModel
 
 @Composable
 fun ChatScreen(navController: NavController,
@@ -86,13 +86,45 @@ fun ChatScreenGenerate(navController: NavController,
    Scaffold(
        modifier = Modifier.fillMaxSize(),
        topBar = {
-           Row(modifier = Modifier.fillMaxWidth().background(Color.LightGray, RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp))) {
-               if (profileUrl=="no"){
-                   Image(modifier=Modifier.size(70.dp),painter = painterResource(id = R.drawable.default_profile_photo), contentDescription ="user's profile photo" )
-               }else{
-                   Image(modifier=Modifier.size(70.dp),painter = rememberAsyncImagePainter(model = profileUrl), contentDescription ="user's profile photo" )
+           Row(modifier = Modifier
+               .fillMaxWidth()
+               .background(
+                   Color.LightGray,
+                   RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp)
+               )) {
+
+               Box(
+                   modifier = Modifier
+                       .size(60.dp)
+                       .border(0.dp, Color.Black, CircleShape)
+
+
+               ) {
+                   if (profileUrl=="no"){
+                       Image(
+                           painter = painterResource(id = R.drawable.default_profile_photo),
+                           contentDescription = "Profile Picture",
+                           modifier = Modifier
+                               .fillMaxSize()
+                               .clip(CircleShape)
+                               .border(0.dp, Color.Black, CircleShape),
+                           contentScale = ContentScale.FillBounds,
+                       )
+                   }else{
+                       Image(
+                           painter = rememberAsyncImagePainter(model = profileUrl),
+                           contentDescription = "Profile Picture",
+                           modifier = Modifier
+                               .fillMaxSize()
+                               .clip(CircleShape)
+                               .border(0.dp, Color.Black, CircleShape),
+                           contentScale = ContentScale.FillBounds,
+                       )
+                   }
                }
-               Text(text = otherUserNick)
+
+
+               Text( modifier = Modifier.padding(top = 15.dp), text = otherUserNick)
            }
        }
             ) {
@@ -128,7 +160,7 @@ fun ChatScreenGenerate(navController: NavController,
 
                    ) {
                    items(items = messageList.reversed()) { message ->
-                       MessageBuble(
+                       MessageBubble(
                            message = message,
                            userNick = userNick
                        )
@@ -172,8 +204,31 @@ fun ChatScreenGenerate(navController: NavController,
 
 
 @Composable
-fun MessageBuble(message: Message,userNick:String){
-    Text(text = "${message.sender} : ${message.text}")
+fun MessageBubble(message: Message, userNick:String){
+    Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxSize()){
+        if (userNick==message.sender){
+            Spacer(modifier = Modifier.size(width = 60.dp, height = 10.dp))
+        }
+        Row( modifier = Modifier
+            .padding(vertical = 5.dp, horizontal = 5.dp)
+            .background(
+                if (userNick == message.sender) {
+                    Color.Cyan
+                } else {
+                    Color.Green
+                }, shape = RoundedCornerShape(10.dp)
+            )
+        ) {
+                Text(modifier=Modifier.padding(5.dp),
+                    text = message.text
+                )
+        }
+
+
+        if (userNick!=message.sender){
+            Spacer(modifier = Modifier.size(width = 60.dp, height = 10.dp))
+        }
+    }
 }
 
 
